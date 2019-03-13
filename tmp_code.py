ea0560c1a80708f173001a797619e8386d600aaf
gregunz <mail@gregunz.io>
2019-03-13 15:32:52 +0100

    move some tmp_code

--- tmp_code.py
+++ tmp_code.py
@@ -27,3 +27,89 @@ def my_collate(batch):
     batch_collated_no_names = default_collate(batch)
     batch_collated_no_names['label_names'] = lbl_names
     return batch_collated_no_names
+
+
+
+class ToTensor(object):
+    """Convert ndarrays in sample to Tensors."""
+
+    def __call__(self, sample):
+        image, label = sample['image'], sample['label']
+
+        # swap color axis because
+        # numpy image: H x W x C
+        # torch image: C X H X W
+        image = image.transpose((2, 0, 1))
+
+        sample = sample.copy()
+        sample['image'] = torch.from_numpy(image)
+        sample['label'] = torch.from_numpy(label)
+        return sample
+
+class RandomCrop(object):
+    """Crop randomly the image in a sample.
+
+    Args:
+        output_size (tuple or int): Desired output size. If int, square crop
+            is made.
+    """
+
+    def __init__(self, output_size):
+        assert isinstance(output_size, (int, tuple))
+        if isinstance(output_size, int):
+            self.output_size = (output_size, output_size)
+        else:
+            assert len(output_size) == 2
+            self.output_size = output_size
+
+    def __call__(self, sample):
+        image, label = sample['image'], sample['label']
+
+        h, w = image.shape[:2]
+        new_h, new_w = self.output_size
+
+        top = np.random.randint(0, h - new_h)
+        left = np.random.randint(0, w - new_w)
+
+        image = image[top: top + new_h, left: left + new_w]
+        label = label[top: top + new_h, left: left + new_w]
+
+        sample = sample.copy()
+        sample['image'] = image
+        sample['label'] = label
+        return sample
+
+class Rescale(object):
+    """Rescale the image in a sample to a given size.
+
+    Args:
+        output_size (tuple or int): Desired output size. If tuple, output is
+            matched to output_size. If int, smaller of image edges is matched
+            to output_size keeping aspect ratio the same.
+    """
+
+    def __init__(self, output_size):
+        assert isinstance(output_size, (int, tuple))
+        self.output_size = output_size
+
+    def __call__(self, sample):
+        image, label = sample['image'], sample['label']
+
+        h, w = image.shape[:2]
+        if isinstance(self.output_size, int):
+            if h > w:
+                new_h, new_w = self.output_size * h / w, self.output_size
+            else:
+                new_h, new_w = self.output_size, self.output_size * w / h
+        else:
+            new_h, new_w = self.output_size
+
+        new_h, new_w = int(new_h), int(new_w)
+
+        img = skimage.transform.resize(image, (new_h, new_w))
+        lbl = skimage.transform.resize(label, (new_h, new_w), preserve_range=True).astype('uint8')
+
+        sample = sample.copy()
+        sample['image'] = img
+        sample['label'] = lbl
+        return sample
