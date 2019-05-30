b17c0a88fe6eafc14081b84493ab7c5c10797889
Gregoire Clement <gregoire.clement@epfl.ch>
2019-05-30 12:20:12 +0200

    adaption of changes to the run script

--- facade_project/scripts/run.py
+++ facade_project/scripts/run.py
@@ -1,19 +1,20 @@
 import argparse
 import datetime
-import json
 
 import pytz
 import torch.optim as optim
 from tensorboardX import SummaryWriter
 
 from facade_project import SIGMA_FIXED, IS_SIGMA_FIXED, SIGMA_SCALE, PATH_TO_DATA, LABEL_NAME_TO_VALUE, \
-    FACADE_ROT_HEATMAPS_INFOS_PATH, FACADE_ROT_DIR
+    FACADE_ROT_DIR
 from facade_project.data import FacadeRandomRotDataset, TransformedDataset, split, to_dataloader
 from facade_project.data.augmentation import random_brightness_and_contrast, random_crop, random_flip, compose
 from facade_project.geometry.heatmap import build_heatmaps
 from facade_project.nn.losses import facade_criterion
+from facade_project.nn.metrics import FacadeMetric
 from facade_project.nn.models import UNet, AlbuNet
 from facade_project.nn.train import train_model
+from facade_project.utils.load import HEATMAP_INFOS_PER_ROT
 
 
 def run_name(model_name, predictions, is_sigma_fixed, sigma_fixed, sigma_scale):
@@ -33,10 +34,8 @@ def run_name(model_name, predictions, is_sigma_fixed, sigma_fixed, sigma_scale):
 
 
 def main(args):
-    heatmaps_infos_per_rot = json.load(open(FACADE_ROT_HEATMAPS_INFOS_PATH, mode='r'))
-
     def create_heatmaps(img_idx, rot_idx):
-        info = heatmaps_infos_per_rot[img_idx][rot_idx]
+        info = HEATMAP_INFOS_PER_ROT[img_idx][rot_idx]
         return build_heatmaps(
             heatmap_info=info,
             label_name_to_value=LABEL_NAME_TO_VALUE,
@@ -46,7 +45,9 @@ def main(args):
             heatmap_types=args.predictions
         )
 
-    with_heatmaps = 'center' in args.predictions or 'size' in args.predictions
+    with_heatmaps = 'center' in args.predictions \
+                    or 'width' in args.predictions \
+                    or 'height' in args.predictions
 
     facade_dataset = FacadeRandomRotDataset(
         img_dir=FACADE_ROT_DIR,
@@ -111,11 +112,11 @@ def main(args):
         criterion=facade_criterion,
         optimizer=optimizer,
         scheduler=None,  # optim.lr_scheduler.StepLR(optimizer, step_size=7, gamma=0.1),
+        metric_handler=FacadeMetric(args.predictions, LABEL_NAME_TO_VALUE),
         writer=SummaryWriter('runs/{}.log'.format(run_name_str)),
         num_epoch=args.epochs,
         keep_n_best=3,
         verbose=True,
-        label_name_to_value=LABEL_NAME_TO_VALUE
     )
 
 
@@ -130,11 +131,12 @@ if __name__ == '__main__':
     parser.add_argument('--batch-train', action="store", dest="batch_train", type=int, default=1)
     parser.add_argument('--batch-val', action="store", dest="batch_test", type=int, default=1)
     parser.add_argument('--predictions', action='store', dest='predictions', nargs='+', type=str,
-                        default=['center', 'size', 'mask'])
+                        default=['mask', 'center', 'height', 'width'])
     parser.add_argument('--is-sigma-fixed', action='store', dest='is_sigma_fixed', type=bool, default=IS_SIGMA_FIXED)
     parser.add_argument('--sigma-fixed', action='store', dest='sigma_fixed', type=float, default=SIGMA_FIXED)
     parser.add_argument('--sigma-scale', action='store', dest='sigma_scale', type=float, default=SIGMA_SCALE)
-    parser.add_argument('--path-for-weights', action='store', dest='path_for_weights', type=str, default=PATH_TO_DATA)
+    parser.add_argument('--path-for-weights', action='store', dest='path_for_weights', type=str,
+                        default='{}/models'.format(PATH_TO_DATA))
     parser.add_argument('--device', action='store', dest='device', type=str, default='cuda:0')
     # parser.add_argument('--', action='store', dest='', type=, default=)
 
