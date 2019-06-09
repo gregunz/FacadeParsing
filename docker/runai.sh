1a7fed6b2f607549a55a10e9cac57d60413540ab
Gregoire Clement <gregoire@hmpgpu.datascience.ch>
2019-06-09 19:05:24 +0200

    scripts to run docker added

---
+++ docker/runai.sh
@@ -0,0 +1,9 @@
+docker run -d \
+  --runtime=nvidia \
+  --name ai \
+  -p 9422:22 \
+  -p 9488:8888 \
+  -v ~/projects:/opt/app/projects \
+  -v /data/facade_segmentation:/data \
+  -v ~/.ssh:/root/.ssh \
+  gregunz/jupyterlab:facade_project
