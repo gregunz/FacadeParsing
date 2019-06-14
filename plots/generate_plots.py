7f311d5343e2f45d05ef140b6bb94024c0fded98
Gregoire Clement <gregoire.clement@epfl.ch>
2019-06-14 18:36:24 +0200

    script to generate plots

---
+++ plots/generate_plots.py
@@ -0,0 +1,33 @@
+import pandas as pd
+from matplotlib import pyplot as plt
+
+# Validation Jaccard Index per beta
+
+f, ax = plt.subplots(figsize=(8, 8))
+ax.set(xscale="log")
+df = pd.read_csv('/data/plots/jaccard_indices_per_beta.csv')
+df.plot(x='beta', ax=ax)
+plt.ylabel('jaccard index')
+plt.title('Validation Jaccard Index per beta')
+plt.savefig('/data/plots/jaccard_indices_per_beta.pdf')
+
+# Validation Loss per model
+
+f, ax = plt.subplots(figsize=(8, 8))
+ax.set(yscale="log")
+df = pd.read_csv('/data/plots/validation_losses.csv')
+df.plot(x='epoch', ax=ax)
+plt.title('Validation Loss per model')
+plt.ylabel('loss')
+plt.savefig('/data/plots/validation_losses.pdf')
+
+# Validation Loss per model
+
+f, ax = plt.subplots(figsize=(8, 8))
+ax.set(yscale="log")
+df = pd.read_csv('/data/plots/train_losses.csv')
+df.plot(x='epoch', ax=ax)
+plt.title('Validation Loss per model')
+plt.ylabel('loss')
+
+plt.savefig('/data/plots/train_losses.pdf')
