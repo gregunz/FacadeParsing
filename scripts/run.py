fd5004c280320e7e3861def8247155f4e22bdeba
Gregoire Clement <gregoire.clement@epfl.ch>
2019-06-15 10:51:08 +0200

    Update scripts

--- scripts/run.py
+++ scripts/run.py
@@ -7,11 +7,12 @@ import pytz
 import torch
 import torch.optim as optim
 from tensorboardX import SummaryWriter
+from torch.utils.data import Subset
 
 from facade_project import PATH_TO_DATA, LABEL_NAME_TO_VALUE, \
     FACADE_ROT_IMAGES_TENSORS_DIR, FACADE_ROT_MEAN, FACADE_ROT_STD, CROP_SIZE, \
-    DEFAULT_SEED_SPLIT, HEATMAP_LABELS
-from facade_project.data import FacadeRandomRotDataset, TransformedDataset, split, to_dataloader
+    DEFAULT_SEED_SPLIT, HEATMAP_LABELS, NUM_IMAGES
+from facade_project.data import FacadeRandomRotDataset, TransformedDataset, to_dataloader, get_indices_split
 from facade_project.data.augmentation import random_brightness_and_contrast, random_crop, random_flip, compose
 from facade_project.data.facade_random_rot_dataset import add_heatmaps_target
 from facade_project.nn.losses import facade_criterion
@@ -55,28 +56,32 @@ def main(args):
     weights_dir_path = '{}/{}'.format(args.path_for_weights, run_name_str)
 
     with_heatmaps = 'heatmaps' in args.predictions
+    train_indices, val_indices = get_indices_split(NUM_IMAGES, seed=args.split_seed, percentage=0.9)
 
     def create_heatmaps(img_idx, rot_idx, device):
         if not with_heatmaps:
             return dict()
         return add_heatmaps_target(img_idx, rot_idx, device)
 
-    facade_dataset = FacadeRandomRotDataset(
-        img_dir=FACADE_ROT_IMAGES_TENSORS_DIR,
-        add_targets_fn=create_heatmaps,
-        img_to_num_rot=None,
-        caching=False,  # when true it takes quite a lot of RAM
-        init_caching=False,
-        device=device,
-    )
+    def build_dataset(is_train):
+        dset = FacadeRandomRotDataset(
+            img_dir=FACADE_ROT_IMAGES_TENSORS_DIR,
+            add_targets_fn=create_heatmaps,
+            img_to_num_rot=None if is_train else [1 for _ in range(NUM_IMAGES)],  # no rotations for validation
+            caching=False,  # when true it takes quite a lot of RAM
+            init_caching=False,
+            device=device,
+        )
+        mean = torch.tensor(FACADE_ROT_MEAN).view(3, 1, 1).to(device)
+        std = torch.tensor(FACADE_ROT_STD).view(3, 1, 1).to(device)
+        dset_normed = TransformedDataset(
+            dataset=dset,
+            transform=lambda img, lbl: ((img - mean) / std, lbl)
+        )
+        return Subset(dset_normed, train_indices if is_train else val_indices)
 
-    mean = torch.tensor(FACADE_ROT_MEAN).view(3, 1, 1).to(device)
-    std = torch.tensor(FACADE_ROT_STD).view(3, 1, 1).to(device)
-    dataset_norm = TransformedDataset(
-        dataset=facade_dataset,
-        transform=lambda img, lbl: ((img - mean) / std, lbl))
-
-    dataset_train, dataset_val = split(dataset_norm, seed=args.split_seed)
+    train_dset = build_dataset(is_train=True)
+    val_dset = build_dataset(is_train=False)
 
     augmentation_tf = compose([
         random_crop(crop_size=args.crop_size),  # Cropping (for batches of same sizes)
@@ -84,20 +89,20 @@ def main(args):
         random_flip(),  # Flipping (augmentation)
     ])
 
-    augmented_train = TransformedDataset(
-        dataset=dataset_train,
+    train_dset_aug = TransformedDataset(
+        dataset=train_dset,
         transform=augmentation_tf,
     )
 
-    dataloaders = {'train': to_dataloader(augmented_train, args.batch_train),
-                   'val': to_dataloader(dataset_val, args.batch_val)}
+    dataloaders = {'train': to_dataloader(train_dset_aug, args.batch_train),
+                   'val': to_dataloader(val_dset, args.batch_val, shuffle=False)}
 
     num_target_channels = 0
     for p in args.predictions:
         if p == 'mask':
             num_target_channels += len(label_name_to_value)
         elif p == 'heatmaps':
-            num_target_channels += 2  # heatmaps for width and height
+            num_target_channels += 3  # heatmaps for width and height
     assert num_target_channels > 0, 'model should predict at least one thing'
 
     if args.load_trained_name is not None and args.load_trained_epoch is not None:
@@ -120,20 +125,18 @@ def main(args):
             n_classes=num_target_channels,
             wf=args.wf,
             padding=True,
-            batch_norm=True,
             up_mode='upconv'
         )
     else:
         raise Exception('model undefined')
 
-    optimizer = optim.Adam(model.parameters(), lr=0.001, betas=(0.9, 0.999))
+    optimizer = optim.Adam(model.parameters(), lr=args.lr, betas=(0.9, 0.999))
     criterion = facade_criterion(
         predictions_list=args.predictions,
         predictions_weights=args.pred_weights,
         num_classes=len(label_name_to_value),
         device=device,
-        use_dice=args.use_dice,
-        center_factor=args.center_factor,
+        use_dice=not args.use_ce,
     )
 
     # make directory for model weights
@@ -155,11 +158,10 @@ def main(args):
             device=device,
             criterion=criterion,
             optimizer=optimizer,
-            scheduler=optim.lr_scheduler.StepLR(optimizer, step_size=7, gamma=0.1),
+            scheduler=optim.lr_scheduler.StepLR(optimizer, step_size=7, gamma=0.1) if args.use_scheduler else None,
             metric_handler=FacadeMetric(args.predictions, label_name_to_value, HEATMAP_LABELS),
             writer=SummaryWriter('runs/{}'.format(run_name_str)),
             num_epoch=args.epochs,
-            # keep_n_best=10,
             verbose=True,
         )
 
@@ -170,19 +172,20 @@ if __name__ == '__main__':
     parser.add_argument('--model', action="store", dest="model", type=str, default='albunet')
     parser.add_argument('--epochs', action="store", dest="epochs", type=int, default=50)
     parser.add_argument('--split-seed', action="store", dest="split_seed", type=int, default=DEFAULT_SEED_SPLIT)
-    parser.add_argument('--batch-train', action="store", dest="batch_train", type=int, default=1)
+    parser.add_argument('--batch-train', action="store", dest="batch_train", type=int, default=4)
     parser.add_argument('--batch-val', action="store", dest="batch_val", type=int, default=1)
     parser.add_argument('--wf', action='store', dest='wf', type=int, default=5)
-    parser.add_argument('--pretrained', action='store', dest='pretrained', type=bool, default=True)
+    parser.add_argument('--lr', action='store', dest='lr', type=float, default=0.001)
+    parser.add_argument('--not-pretrained', action='store_false', dest='pretrained', default=True)
     parser.add_argument('--predictions', action='store', dest='predictions', nargs='+', type=str,
                         default=['mask', 'heatmaps'])
     parser.add_argument('--pred-weights', action='store', dest='pred_weights', nargs='+', type=float, default=None)
     parser.add_argument('--path-for-weights', action='store', dest='path_for_weights', type=str,
                         default='{}/models'.format(PATH_TO_DATA))
     parser.add_argument('--device', action='store', dest='device', type=str, default='cuda:0')
-    parser.add_argument('--use-dice', action='store', dest='use_dice', type=bool, default=True)
+    parser.add_argument('--use-ce', action='store_true', dest='use_ce', default=False)
+    parser.add_argument('--use-scheduler', action='store_true', dest='use_scheduler', default=False)
     parser.add_argument('--crop-size', action='store', dest='crop_size', type=int, default=CROP_SIZE)
-    parser.add_argument('--center-factor', action='store', dest='center_factor', type=float, default=90.0)
     parser.add_argument('--load-trained-name', action='store', dest='load_trained_name', type=str, default=None)
     parser.add_argument('--load-trained-epoch', action='store', dest='load_trained_epoch', type=int, default=None)
     #  parser.add_argument('--', action='store', dest='', type=int, default=)
--- scripts/run.sh
+++ scripts/run.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-python3 run.py --epochs=50 --device=cuda:${1:-0} --model=${2:-albunet} --batch-train=${3:-4} --predictions mask
+# different weights for the loss
+python3 run.py --epochs=50 --device=cuda:${1:-0} --model=${2:-albunet} --batch-train=${3:-4} --pred-weights 1 0.0001
+python3 run.py --epochs=50 --device=cuda:${1:-0} --model=${2:-albunet} --batch-train=${3:-4} --pred-weights 1 0.0002
 python3 run.py --epochs=50 --device=cuda:${1:-0} --model=${2:-albunet} --batch-train=${3:-4} --pred-weights 1 0.0005
 python3 run.py --epochs=50 --device=cuda:${1:-0} --model=${2:-albunet} --batch-train=${3:-4} --pred-weights 1 0.001
 python3 run.py --epochs=50 --device=cuda:${1:-0} --model=${2:-albunet} --batch-train=${3:-4} --pred-weights 1 0.002
@@ -12,4 +14,7 @@ python3 run.py --epochs=50 --device=cuda:${1:-0} --model=${2:-albunet} --batch-t
 python3 run.py --epochs=50 --device=cuda:${1:-0} --model=${2:-albunet} --batch-train=${3:-4} --pred-weights 1 0.2
 python3 run.py --epochs=50 --device=cuda:${1:-0} --model=${2:-albunet} --batch-train=${3:-4} --pred-weights 1 0.5
 python3 run.py --epochs=50 --device=cuda:${1:-0} --model=${2:-albunet} --batch-train=${3:-4} --pred-weights 1 1
-python3 run.py --epochs=50 --device=cuda:${1:-0} --model=${2:-albunet} --batch-train=${3:-4} --predictions heatmaps
+
+# only mask or heatmaps
+# python3 run.py --epochs=50 --device=cuda:${1:-0} --model=${2:-albunet} --batch-train=${3:-4} --predictions heatmaps
+# python3 run.py --epochs=50 --device=cuda:${1:-0} --model=${2:-albunet} --batch-train=${3:-4} --predictions mask
