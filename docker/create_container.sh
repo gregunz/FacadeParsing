ead4f5e37741f2756f3891377163483746703920
Gregoire Clement <gregoire.clement@epfl.ch>
2019-06-15 10:43:51 +0200

    Add comments

--- docker/create_container.sh
+++ docker/create_container.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
+# 9422:22 port if you want to use ssh connection to your docker (e.g. useful for remote interpreter in PyCharm)
+# 9488:8888 port to use jupyterlab/jupyter notebook
+# ssh keys added to commit on git
+# remove/add/change volumes depending on your needs
 
 docker run -d \
   --runtime=nvidia \
